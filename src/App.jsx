cd5ed43f51049f15042c64064d03a4f931f7468a
Antkilla <132300802+Antkilla@users.noreply.github.com>
2023-10-18 19:03:12 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import SingleCard from './components/SingleCard'
 
 const cardImages = [
 {"src":"/img/tiger.jpg"},
@@ -13,12 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
-
-   // Initialize cards with the back image visible
-  const initializeCards = () => {
-    const initialCards = cardImages.map((card) => ({ ...card, id: Math.random(), isFlipped: false }));
-    setCards(initialCards);
-  };
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   //randomize cards
   const shuffleCards = () =>  {
@@ -30,36 +27,22 @@ function App() {
     setTurns(0)
   }
 
-   // Handle card click
-  const handleCardClick = (clickedCard) => {
-   // Toggle the isFlipped state of the clicked card
-    setCards((prevCards) =>
-      prevCards.map((card) =>
-        card.id === clickedCard.id ? { ...card, isFlipped: !card.isFlipped } : card
-    )
-  );
-
-  // Increment the turns
-  setTurns((prevTurns) => prevTurns + 1);
-};
-
+  //handel a choice funct, make prop then pass to single card
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
   return (
     <div className="App">
       <h1>Magic Memory</h1>
-      <button onClick={initializeCards}>New Game</button>
-      <button onClick={shuffleCards}>Shuffle Cards</button>
+      <button onClick={shuffleCards}>New Game</button>
     
       <div className="card-grid">
         {cards.map(card => (
-          <div key={card.id}
-            className={`card ${card.isFlipped ? 'flipped' : ''}`}
-            onClick={() => handleCardClick(card)}>
-            <div>
-              <img className="front" src={card.isFlipped ? card.src : '/img/crown.jpg'}  alt="card front"/>
-              <img className="back" src="/img/crown.jpg" alt="card back" />
-            </div>
-          </div>
+          <SingleCard 
+          key={card.id} 
+          card={card}
+          handleChoice={handleChoice}/>
         ))}
       </div>
     </div>
